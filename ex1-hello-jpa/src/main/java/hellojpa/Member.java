4ea727f6e87fab930ba91a3f299ea7363c21ec49
EJ <hanej93@naver.com>
2022-10-12 21:38:48 +0900

    first commit

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,47 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+//    @ManyToOne
+//    @JoinColumn(name = "TEAM_ID")
+//    private Team team;
+//
+//    @OneToOne
+//    @JoinColumn(name = "LOCKER_ID")
+//    private Locker locker;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+//    public Team getTeam() {
+//        return team;
+//    }
+//
+//    public void setTeam(Team team) {
+//        this.team = team;
+//    }
+}
