2bd9595aa2844099d17b70698a3543c0b604878b
EJ <hanej93@naver.com>
2023-01-14 02:00:14 +0900

    값 타입의 비교

--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -2,12 +2,15 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-        String a = "내용1";
-        String b = a;
+        int a = 10;
+        int b = 10;
 
-        a = "내용2";
+        System.out.println("a = b : " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+
+        System.out.println("(address1 == address2) = " + (address1 == address2));
+        System.out.println("address1.equals(address2) = " + address1.equals(address2));
     }
 }
